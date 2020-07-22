4a80f915016d5a7e275314eacd47cec61731be82
chaaaron000 <bjpowerll12967@gmail.com>
2020-07-22 22:39:41 +0900

    2020.07.22 Second GUI code commit

--- gui.py
+++ gui.py
@@ -14,22 +14,13 @@ class MyApp(QWidget):
         grid = QGridLayout()
 
         grid.addWidget(QLabel("히토미 다운로더 저장 폴더:"), 0, 0)
+
         grid.addWidget(QLabel("저장 폴더:"), 1, 0)
+
         clean_button = QPushButton("Clean Up")
         grid.addWidget(clean_button, 2, 0)
 
-        # hbox = QHBoxLayout()
-        # hbox.addStretch(1)
-        # hbox.addWidget(clean_button)
-        # hbox.addStretch(1)
-
-        # vbox = QVBoxLayout()
-        # vbox.addStretch(1)
-        # vbox.addLayout(hbox)
-        # vbox.addStretch(1)
-
         self.setLayout(grid)
-        # self.setLayout(vbox)
 
 
 if __name__ == "__main__":
