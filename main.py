9744d71c15978ac6f10c7308d9ecbe2c70708657
chaaaron000 <bjpowerll12967@gmail.com>
2020-07-28 08:09:39 +0900

    Done without style

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from PyQt5.QtWidgets import QApplication, QWidget, QDesktopWidget, QPushButton, 
 
 
 class MyApp(QWidget):
+    TARGET_PATH = ""
+    GOAL_PATH = ""
 
     def __init__(self):
         super().__init__()
@@ -23,15 +25,14 @@ class MyApp(QWidget):
         self.move(qr.topLeft())
 
     def init_Ui(self):
-        first_path_button = QPushButton("...", self)
-        first_path_button.clicked.connect(
-            self.get_path)
+        self.first_path_button = QPushButton("...", self)
+        self.first_path_button.clicked.connect(self.get_target_path)
         self.first_label = QLabel("다운받은 폴더가 있는 디렉토리를 선택해주세요.", self)
-        second_path_button = QPushButton("...", self)
-        second_path_button.clicked.connect(self.get_path)
+        self.second_path_button = QPushButton("...", self)
+        self.second_path_button.clicked.connect(self.get_goal_path)
         self.second_label = QLabel("분류한 폴더를 저장할 디렉토리를 선택해주세요.", self)
-        clean_button = QPushButton("Clean Up", self)
-        clean_button.clicked.connect(self.clean)
+        self.clean_button = QPushButton("Clean Up", self)
+        self.clean_button.clicked.connect(self.clean)
         self.msg = QMessageBox()
         self.msg.setWindowTitle("Error")
         self.msg.setText("경로를 선택해주세요!")
@@ -40,18 +41,24 @@ class MyApp(QWidget):
         self.setLayout(grid)
 
         grid.addWidget(self.first_label, 0, 0)
-        grid.addWidget(first_path_button, 0, 1)
+        grid.addWidget(self.first_path_button, 0, 1)
         grid.addWidget(self.second_label, 1, 0)
-        grid.addWidget(second_path_button, 1, 1)
-        grid.addWidget(clean_button, 3, 0)
+        grid.addWidget(self.second_path_button, 1, 1)
+        grid.addWidget(self.clean_button, 3, 0)
 
-    def get_path(self):
-        result = str(QFileDialog.getExistingDirectory())
-        print(result)
-        return result
+    def get_target_path(self):
+        self.TARGET_PATH = str(QFileDialog.getExistingDirectory())
+        self.first_label.setText(self.TARGET_PATH)
+
+    def get_goal_path(self):
+        self.GOAL_PATH = str(QFileDialog.getExistingDirectory())
+        self.second_label.setText(self.GOAL_PATH)
 
     def clean(self):
-        pass
+        if self.TARGET_PATH != "" and self.GOAL_PATH != "":
+            clean_up(self.TARGET_PATH, self.GOAL_PATH)
+        else:
+            self.msg.exec_()
 
 
 if __name__ == "__main__":
